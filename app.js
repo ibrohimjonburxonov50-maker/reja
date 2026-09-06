fd3153dd8107c4005f7e26905df2a7abdde15d8a
ibrohimjon <ibrohimjonburxonov50@gmail.com>
2026-09-06 19:45:47 +0900

    feat : frontEnd javascriptni qurish

--- app.js
+++ app.js
@@ -24,15 +24,23 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
+// app.post("/create-item", (req, res) => {
+//   console.log("user entered /create-item");
+//   const new_reja = req.body.reja;
+//   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+//     console.log(data.ops);
+//    res.json(data.ops[0]);
+//   });
+// });
 app.post("/create-item", (req, res) => {
   console.log("user entered /create-item");
   const new_reja = req.body.reja;
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
     if (err) {
       console.log(err);
-      res.end("something went wrong");
+      res.json({ state: "something went wrong" });
     } else {
-      res.end("successfully added");
+      res.json(data.ops[0]);
     }
   });
 });
